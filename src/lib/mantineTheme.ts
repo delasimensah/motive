0adc17732b89f3ae45fd97412b2571fea104081c
delasimensah <mensadelasi@gmail.com>
2023-02-07 13:11:30 +0000

    initial project setup

---
+++ src/lib/mantineTheme.ts
@@ -0,0 +1,31 @@
+import { MantineThemeOverride, ColorScheme } from "@mantine/core";
+
+const getTheme = (
+  colorScheme: ColorScheme,
+  fontFamily: string,
+  headerFontFamily: string
+): MantineThemeOverride => {
+  return {
+    colorScheme,
+    fontFamily,
+    headings: { fontFamily: headerFontFamily },
+    // colors: {
+    //   brand: [
+    //     "#F0BBDD",
+    //     "#ED9BCF",
+    //     "#EC7CC3",
+    //     "#ED5DB8",
+    //     "#F13EAF",
+    //     "#F71FA7",
+    //     "#FF00A1",
+    //     "#E00890",
+    //     "#C50E82",
+    //     "#AD1374",
+    //   ],
+    // },
+    // primaryColor: "brand",
+    // primaryShade: { light: 6, dark: 8 },
+  };
+};
+
+export default getTheme;
---
+++ src/pages/_app.tsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { GetServerSidePropsContext } from "next";
+import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo";
+import { Figtree } from "@next/font/google";
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from "@mantine/core";
+import getTheme from "@lib/mantineTheme";
+import { getCookie, setCookie } from "cookies-next";
+
+import "@styles";
+
+const font = Figtree({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-figtree",
+});
+
+const App = (props: AppProps & { colorScheme: ColorScheme }) => {
+  const { Component, pageProps } = props;
+
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
+
+  const toggleColorScheme = () => {
+    const nextColorScheme = colorScheme === "dark" ? "light" : "dark";
+
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme);
+  };
+
+  const theme = getTheme(
+    colorScheme,
+    font.style.fontFamily,
+    font.style.fontFamily
+  );
+
+  return (
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider theme={theme} withGlobalStyles>
+        <style jsx global>{`
+          html {
+            font-family: ${font.style.fontFamily};
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            font-family: ${font.style.fontFamily};
+          }
+        `}</style>
+
+        <DefaultSeo
+          title="Application or website name"
+          description="Description here"
+        />
+
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  );
+};
+
+App.getInitialProps = ({ ctx }: { ctx: GetServerSidePropsContext }) => ({
+  colorScheme: getCookie("mantine-color-scheme", ctx) || "light",
+});
+
+export default App;
