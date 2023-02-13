a3f58fe989a9229d842586e009caede2c6ddd6e3
delasimensah <mensadelasi@gmail.com>
2023-02-13 18:54:01 +0000

    began building auth pages

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -1,10 +1,15 @@
-import { Text } from "@mantine/core";
+import { Text, Button } from "@mantine/core";
+import Link from "next/link";
 
 const SignInPage = () => {
   return (
-    <>
-      <Text>SignIn</Text>
-    </>
+    <div className="auth-wrapper">
+      <Text>Welcome Back</Text>
+      <Link href="/sign-up">Sign Up</Link>
+      <Button fullWidth className="bg-purple hover:bg-purple">
+        Sign In
+      </Button>
+    </div>
   );
 };
 export default SignInPage;
--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,10 +1,12 @@
 import { Text } from "@mantine/core";
+import Link from "next/link";
 
 const SignUpPage = () => {
   return (
-    <>
-      <Text>SignUp</Text>
-    </>
+    <div className="auth-wrapper">
+      <Text>Let&apos;s Get Started</Text>
+      <Link href="/sign-in">Sign In</Link>
+    </div>
   );
 };
 export default SignUpPage;
