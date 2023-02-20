c028e1b4e6f217ec65422f599474c24669ff8d35
delasimensah <mensadelasi@gmail.com>
2023-02-20 11:53:14 +0000

    added layout for dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,28 @@ import { Text } from "@mantine/core";
 
 const HomePage = () => {
   return (
-    <>
-      <Text>Home</Text>
-    </>
+    <div className="space-y-[30px]">
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-[30px]">
+        <div className="h-[270px] bg-purple rounded-lg"></div>
+        <div className="h-[270px] bg-white rounded-lg"></div>
+        <div className="h-[270px] bg-white rounded-lg"></div>
+        <div className="h-[270px] bg-white rounded-lg"></div>
+      </div>
+
+      <div className="grid lg:grid-cols-2 gap-[30px]">
+        <div className="h-[350px] bg-white rounded-lg"></div>
+
+        <div className="h-[350px] bg-white rounded-lg"></div>
+      </div>
+
+      <div className="grid md:grid-cols-3 gap-[30px]">
+        <div className="h-[240px] bg-[#e1dfa4] rounded-lg"></div>
+
+        <div className="h-[240px] bg-[#e3ecf1] rounded-lg"></div>
+
+        <div className="h-[240px] bg-[#f4e3e5] rounded-lg"></div>
+      </div>
+    </div>
   );
 };
 
