4474712abb4b94c13ddfa9f74a79cd35033a2d82
delasimensah <mensadelasi@gmail.com>
2023-02-20 12:32:19 +0000

    added dark theme to dashboard page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,23 @@
-import { Text } from "@mantine/core";
+import { useMantineColorScheme } from "@mantine/core";
 
 const HomePage = () => {
+  const { colorScheme } = useMantineColorScheme();
+  const dark = colorScheme === "dark";
+  const bgColor = dark ? "bg-dark1" : "bg-white";
+
   return (
     <div className="space-y-[30px]">
       <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-[30px]">
         <div className="h-[270px] bg-purple rounded-lg"></div>
-        <div className="h-[270px] bg-white rounded-lg"></div>
-        <div className="h-[270px] bg-white rounded-lg"></div>
-        <div className="h-[270px] bg-white rounded-lg"></div>
+        <div className={`h-[270px] ${bgColor} rounded-lg`}></div>
+        <div className={`h-[270px] ${bgColor} rounded-lg`}></div>
+        <div className={`h-[270px] ${bgColor} rounded-lg`}></div>
       </div>
 
       <div className="grid lg:grid-cols-2 gap-[30px]">
-        <div className="h-[350px] bg-white rounded-lg"></div>
+        <div className={`h-[350px] ${bgColor} rounded-lg`}></div>
 
-        <div className="h-[350px] bg-white rounded-lg"></div>
+        <div className={`h-[350px] ${bgColor} rounded-lg`}></div>
       </div>
 
       <div className="grid md:grid-cols-3 gap-[30px]">
